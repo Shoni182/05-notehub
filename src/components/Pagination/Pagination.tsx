d8cdf36ca87a6b040b9e17a49e835a7b0d9c7be2
Shoni Pal <100448741+Shoni182@users.noreply.github.com>
2026-02-09 13:05:58 +0200

    add css

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,4 @@
+import css from "./Pagination.module.css";
+// До http-запиту потрібно додати параметри page та perPage. Наприклад:
+// GET https://notehub-public.goit.study/api/notes?page=1&perPage=1
+// Додайте умову, щоб компонент Pagination рендерився лише в тому випадку, якщо кількість сторінок колекції нотаток більше 1.
